04118d5fd5fe72fa4c02625d0d15667936e19c0f
Rich Tibbett <richt@opera.com>
2012-11-19 13:30:28 +0100

    Update build system

--- utils/build_debug.bat
+++ utils/build_debug.bat
@@ -1,2 +1,3 @@
+python build.py --include common --output ../build/operaextensions.js
 python build.py --include common --output ../build/operaextensions.min.js
-python build.py --include wintabs --output ../build/operaextensions_wintabs.min.js
+python build.py --include wintabs --output ../build/operaextensions_wintabs.js
--- utils/build_debug.sh
+++ utils/build_debug.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
+python build.py --include common --output ../build/operaextensions.js
 python build.py --include common --output ../build/operaextensions.min.js
-python build.py --include wintabs --output ../build/operaextensions_wintabs.min.js
+python build.py --include wintabs --output ../build/operaextensions_wintabs.js
