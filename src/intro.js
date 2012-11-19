36479e3db83e5529a1fb063dadf2abc393dd6ac2
richtr <rich.tibbett@gmail.com>
2012-11-19 13:17:28 +0100

    Move root stuff in to intro file

--- src/intro.js
+++ src/intro.js
@@ -1,2 +1,15 @@
 (function( global ) {
 
+  var opera = global.opera || {};
+
+  var OEX = opera.extension = opera.extension || { REVISION: '1' };
+
+  self.console = self.console || {
+
+    info: function() {},
+    log: function() {},
+    debug: function() {},
+    warn: function() {},
+    error: function() {}
+
+  };
