a4f9a0ab8b66b1d785266b12b4018a6e8fdf53ee
richtr <rich.tibbett@gmail.com>
2012-11-20 18:06:45 +0100

    Add o.e.bgProcess to Injected Script and Popup processes

--- src/injectedscript/Root.js
+++ src/injectedscript/Root.js
@@ -7,6 +7,10 @@ var OExtension = function() {
 
 OExtension.prototype = Object.create( OMessagePort.prototype );
 
+OExtension.prototype.__defineGetter__('bgProcess', function() {
+  return chrome.extension.getBackgroundPage();
+});
+
 // Generate API stubs
 
 var OEX = opera.extension = opera.extension || (function() { return new OExtension(); })();
