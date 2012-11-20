ba1ce4933799c2223ad7fd17e53b0297810c9683
richtr <rich.tibbett@gmail.com>
2012-11-20 18:14:31 +0100

    Add opera.postError to all scripts

--- examples/Messaging/operaextensions_background.js
+++ examples/Messaging/operaextensions_background.js
@@ -1,6 +1,11 @@
 !(function( global ) {
 
-  var opera = global.opera || { REVISION: '1' };
+  var opera = global.opera || { 
+    REVISION: '1', 
+    postError: function() { 
+      console.log.apply( null, arguments ); 
+    } 
+  };
   
 var OEvent = function(eventType, eventProperties) {
 
--- src/Header.js
+++ src/Header.js
@@ -1,4 +1,9 @@
 !(function( global ) {
 
-  var opera = global.opera || { REVISION: '1' };
+  var opera = global.opera || { 
+    REVISION: '1', 
+    postError: function() { 
+      console.log.apply( null, arguments ); 
+    } 
+  };
   
