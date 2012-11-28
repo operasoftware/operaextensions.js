2321d75585d8a41991da869f81211abadc03f03c
Łukasz Staliś <lstalis@opera.com>
2012-11-28 14:38:18 +0100

    ResourceLoaderAPI

--- src/utils/File.js
+++ src/utils/File.js
@@ -1,9 +1,38 @@
-OEX.getFile = function(path) {
-	if(!path)retrun;
+OExtension.prototype.getFile = function(path) {
+	var response = null;
 	
-	var xhr = new XMLHttpRequest();
+	if(typeof path != "string")return response;
 	
+  try{
+    var host = chrome.extension.getURL('');
+    
+    if(path.indexOf('widget:')==0)path = path.replace('widget:','chrome-extension:');
+    
+    path = (path.indexOf(host)==-1?host:'')+path;
+    var xhr = new XMLHttpRequest();
+    
+    xhr.onloadend = function(){
+        if (xhr.readyState==xhr.DONE && xhr.status==200){
+          result = xhr.response;
+          
+          result.name = path.substring(path.lastIndexOf('/')+1);
+          
+          result.lastModifiedDate = null;
+          result.toString = function(){
+            return "[object File]";
+          };
+          response = result;
+        };
+    };
+   
+    xhr.open('GET',path,false);
+    xhr.responseType = 'blob';
+  
+    xhr.send(null);
 	
-	
-	
+  } catch(e){
+    return response;
+  };
+  
+	return response;
 };
