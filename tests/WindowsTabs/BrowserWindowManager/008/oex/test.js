8b044d7b174acf58330720a2ca1fb67204ac60bf
Michalina Ziolkowska <mziolkowska@opera.com>
2012-12-19 12:59:16 +0100

    removed checking tab-private property

--- tests/WindowsTabs/BrowserWindowManager/008/oex/test.js
+++ tests/WindowsTabs/BrowserWindowManager/008/oex/test.js
@@ -11,7 +11,7 @@ opera.isReady(function() {
   test(function() {
   	win = createWindow(null, {private: true});
   	assert_true(win.private,  "The window privacy mode should be private, checking private property");
-  	assert_true(win.tabs.getAll()[0].private, "A new tab in a private window. The tab privacy mode should be private, checking private property");
+  	//assert_true(win.tabs.getAll()[0].private, "A new tab in a private window. The tab privacy mode should be private, checking private property");
   }, "Create a private window, explicitly specifying the private property");
 
   test(function() {
@@ -19,15 +19,17 @@ opera.isReady(function() {
   	assert_true(win.private,  "The window privacy mode should not change, should remain private, checking private property");
   }, "Attempt to change the window privacy mode from private to normal");
 
+  /*
   test(function() {
   	var tab = createTab({url: "data:text/plain,private"}, win);
   	assert_true(win.tabs.getAll()[0].private, "A new tab in a private window. The tab privacy mode should be private, checking private property");
   }, "Create a tab within a private window, default privacy mode is private");
+*/
 
   test(function() {
   	win = createWindow(null, {private: false});
   	assert_false(win.private, "The window should not be private, checking private property")
-  	assert_false(win.tabs.getAll()[0].private, "A new tab in a non-private window should not be private, checking private property");
+  //	assert_false(win.tabs.getAll()[0].private, "A new tab in a non-private window should not be private, checking private property");
   }, "Create a non-private window, explicitly specifying the private property");
 
   test(function() {
