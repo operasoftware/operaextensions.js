9f3cdfe415c26cef5c2730af22eb2f153743e313
Rich Tibbett <richt@opera.com>
2013-01-09 15:22:07 +0100

    Initial URL Filter API implementation

--- examples/catalog/close-tabs-right-of-current-0.2.4/includes/userJS.js
+++ examples/catalog/close-tabs-right-of-current-0.2.4/includes/userJS.js
@@ -1,3 +1,8 @@
 opera.isReady(function() {
   // injected script content here (none provided in this extension)
+  
+  opera.extension.urlfilter.addEventListener('contentblocked', function(evt) {
+    console.log('contentblocked event received!');
+    console.log(evt);
+  }, false);
 });
