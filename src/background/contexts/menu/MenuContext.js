a4f666107abce8b1ac476deacc9ba96f25219845
Łukasz Staliś <lstalis@opera.com>
2013-01-09 14:20:57 +0100

    BrowserTab.postMessage fix and ported MenuContext API

---
+++ src/background/contexts/menu/MenuContext.js
@@ -0,0 +1,20 @@
+var MenuContext = function(internal) {
+  chrome.contextMenus.removeAll();//clear all items
+  
+	OMenuContext.apply(this,[internal]);
+  
+  Object.defineProperty(this,'toString',{enumerable: false,  configurable: false, writable: false, value: function(event){
+		return "[object MenuContext]";
+	}});
+  
+};
+
+MenuContext.prototype = Object.create( OMenuContext.prototype );
+
+MenuContext.prototype.createItem = function( menuItemProperties ) {
+  return new MenuItem(Opera, menuItemProperties );
+};
+
+global.MenuContext = MenuContext;
+OEC.menu = OEC.menu || new MenuContext(Opera);
+
--- tests/contextMenu/event/002/injectedtest.js
+++ tests/contextMenu/event/002/injectedtest.js
@@ -1,4 +1,7 @@
+opera.isReady(function() {
+
 opera.contexts.menu.onclick = function(e){
-  alert('click');
-  
+  console.log(e.target.id,e.currentTarget,e.srcElement, e.selectionText);
 };
+
+});
