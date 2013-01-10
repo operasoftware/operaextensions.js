1f7a25b1a5e8b8a9f47d1fd5cc1b565fc1a353de
Łukasz Staliś <lstalis@opera.com>
2013-01-10 15:17:08 +0100

    [CM] permission detection

--- src/injectedscript/contexts/Menu.js
+++ src/injectedscript/contexts/Menu.js
@@ -1,4 +1,4 @@
-if(widget.properties.permissions.indexOf('contextMenus')!=-1){
+if(widget && widget.properties && widget.properties.permissions && widget.properties.permissions.indexOf('contextMenus')!=-1){
 
 OEC.menu = OEC.menu || new MenuContextProxy();
 
