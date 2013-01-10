9c6353f919c9e79c425de065ad656a86721c060d
Rich Tibbett <richt@opera.com>
2013-01-10 16:31:33 +0100

    [WinTabs, Toolbar, URL Filter] permission detection

--- src/background/TabGroups.js
+++ src/background/TabGroups.js
@@ -1,2 +1,6 @@
 
-OEX.tabGroups = OEX.tabGroups || new BrowserTabGroupManager();
+if(widget && widget.properties && widget.properties.permissions && widget.properties.permissions.indexOf('tabs') != -1) {
+
+  OEX.tabGroups = OEX.tabGroups || new BrowserTabGroupManager();
+
+}
--- src/background/Tabs.js
+++ src/background/Tabs.js
@@ -1,2 +1,6 @@
 
-OEX.tabs = OEX.tabs || new RootBrowserTabManager();
+if(widget && widget.properties && widget.properties.permissions && widget.properties.permissions.indexOf('tabs') != -1) {
+
+  OEX.tabs = OEX.tabs || new RootBrowserTabManager();
+
+}
