2379e8e8192f7e4a0792f0f4041e9365e35af01b
Michalina Ziolkowska <mziolkowska@opera.com>
2013-01-11 11:29:19 +0100

    test-lib added to messaging-001

--- tests/messaging/001/oex/popup.js
+++ tests/messaging/001/oex/popup.js
@@ -1,7 +1,7 @@
 opera.isReady(function(){
     window.addEventListener("load", function() {
         opera.extension.onmessage = function(event) {
-    	event.source.postMessage("Hi");
+            event.source.postMessage("Message from popup");
         }
     }, false);
 });
---
+++ tests/messaging/001/oex/test.js
@@ -0,0 +1,46 @@
+﻿opera.isReady(function(){
+
+    var tab = [];
+    var test = async_test("Check back broadCast popup", {timeout: 30000});
+
+    function onMessage(event) {
+
+	test.step(function(){
+	    assert_equals("Message from popup", event.data, "Message received from the popup.");
+	});
+	test.done();
+    }
+    function onConnect(event) {
+	opera.extension.broadcastMessage("Reply to this immediately. All of you");
+    }
+
+    opera.extension.onmessage = onMessage;
+    opera.extension.onconnect = onConnect;
+
+    try {
+	var UIItemProperties = {
+	    disabled: false,
+	    title: "001 - broadcast popup",
+	    icon: "oex/icon.png",
+	    popup: {
+		href: "oex/popup.html"
+	    },
+	    onclick: function(){
+		testStarted = true;
+		console.log('test started');
+	    }
+
+	}
+	theButton = opera.contexts.toolbar.createItem( UIItemProperties );
+	theButton.addEventListener('click', function(){console.log('dupa')});
+	opera.contexts.toolbar.addItem( theButton );
+
+	var uri = "data:text/html,<!DOCTYPE html>" + encodeURIComponent("Please click on the button to load the popup. Once the popup loads, a broadcast will be sent out.");
+	createTab({url: uri, focused: true });
+
+    } catch (e) {
+        tests.step(function() {
+            assert_unreached("Couldn't create button");
+        });
+    }
+});
