03fa34f543d5759e1aef950397c932c2de69b056
richtr <rich.tibbett@gmail.com>
2013-01-14 13:18:23 +0100

    Update build scripts

--- examples/catalog/extendtube-1.16.1/shim/operaextensions_injectedscript.min.js
+++ examples/catalog/extendtube-1.16.1/shim/operaextensions_injectedscript.min.js
@@ -1099,6 +1099,8 @@ OEX.urlfilter = OEX.urlfilter || new UrlFilterEventListener();
 
         // Replace addEventListener for given target
         target.addEventListener = function(name, fn, usecapture) {
+          name = name + ""; // force event name to type string
+          
           if (name.toLowerCase() === _name.toLowerCase()) {
             if (fn === undefined || fn === null ||
                   Object.prototype.toString.call(fn) !== "[object Function]") {
@@ -1129,10 +1131,10 @@ OEX.urlfilter = OEX.urlfilter || new UrlFilterEventListener();
       interceptAddEventListener(global, 'domcontentloaded'); // handled bubbled DOMContentLoaded
       interceptAddEventListener(global.document, 'readystatechange');
 
-      function fireEvent(name, target) {
+      function fireEvent(name, target, props) {
         var evtName = name.toLowerCase();
 
-        var evt = new OEvent(evtName, {});
+        var evt = new OEvent(evtName, props || {});
 
         for (var i = 0, len = fns[evtName].length; i < len; i++) {
           fns[evtName][i].call(target, evt);
@@ -1167,7 +1169,7 @@ OEX.urlfilter = OEX.urlfilter || new UrlFilterEventListener();
               global.document.readyState = 'interactive';
               fireEvent('readystatechange', global.document);
 
-              fireEvent('domcontentloaded', global.document);
+              fireEvent('domcontentloaded', global.document, { bubbles: true }); // indicate that event bubbles
 
               if(currentTime >= domContentLoadedTimeoutOverride) {
                 console.warn('document.domcontentloaded event fired on check timeout');
