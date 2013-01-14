68c7ae59d714246cc3c92f8fc622c7b4028ff31f
richtr <rich.tibbett@gmail.com>
2013-01-14 13:19:22 +0100

    Revert example extension code (/examples/catalog/opera.isReady(function() {)

--- examples/catalog/close-tabs-right-of-current-0.2.4/background.js
+++ examples/catalog/close-tabs-right-of-current-0.2.4/background.js
@@ -1,15 +1,89 @@
 opera.isReady(function() {
 
-  window.onload = function() {
-    console.log('' + (+new Date) + ': Onload fired');
-  };
+  var leftButton, rightButton, extension = opera.extension;
+  var tabs = extension.tabs.getAll ? extension.tabs.getAll() : extension.tabs;
 
-  document.onreadystatechange = function() {
-    console.log('' + (+new Date) + ': Ready state changed: ' + document.readyState);
-  };
+  // for compatibility with Opera 11.x
+  function getFocusedWindow() {
+    return extension.windows.getFocused ? extension.windows.getFocused() : extension.windows.getLastFocused();
+  }
 
-  window.addEventListener("DOMContentLoaded", function() {
-    console.log('' + (+new Date) + ': DOM Content Loaded');
-  });
+  function closeTab(tab) {
+    tabs.close ? tabs.close(tab) : tab.close();
+  }
+
+  function isFocused(tab) {
+    return tab.focused || tab.selected;
+  }
+
+  function closeTabsRightOfCurrent(current_tabs) {
+    var current_tabs_arr = current_tabs.map ? current_tabs : current_tabs.getAll();
+    var i = current_tabs_arr.map(isFocused).indexOf(true);
+    current_tabs_arr.slice(i + 1 - parseInt(widget.preferences.getItem('includeSelf'))).forEach(closeTab);
+  }
+
+  function closeTabsLeftOfCurrent(current_tabs) {
+    var current_tabs_arr = current_tabs.map ? current_tabs : current_tabs.getAll();
+    var i = current_tabs_arr.map(isFocused).indexOf(true);
+    current_tabs_arr.slice(0, i + parseInt(widget.preferences.getItem('includeSelf'))).forEach(closeTab);
+  }
+
+  function setupConnection() {
+    extension.onmessage = function(event) {
+      resetButtons();
+    };
+
+    extension.onconnect = function(event) {
+      event.source.postMessage("hello");
+    };
+  }
+
+  function resetButtons() {
+    opera.contexts.toolbar.removeItem(leftButton);
+    opera.contexts.toolbar.removeItem(rightButton);
+
+    if (widget.preferences.getItem('option') == '1') {
+      opera.contexts.toolbar.addItem(leftButton);
+    }
+    else if (widget.preferences.getItem('option') == '2') {
+      opera.contexts.toolbar.addItem(rightButton);
+    }
+  }
+
+  window.addEventListener('load', function() {
+
+  			  var leftProperties = {
+  			    disabled: false,
+  			    title: "Close tabs left of current",
+  			    icon: "icons/icon_left_18.png"
+  			  };
+
+  			  var rightProperties = {
+  			    disabled: false,
+  			    title: "Close tabs right of current",
+  			    icon: "icons/icon_right_18.png"
+  			  };
+
+  			  leftButton = opera.contexts.toolbar.createItem(leftProperties);
+  			  rightButton = opera.contexts.toolbar.createItem(rightProperties);
+
+  			  leftButton.onclick = function() {
+  			    closeTabsLeftOfCurrent(getFocusedWindow().tabs);
+  			  };
+  			  rightButton.onclick = function() {
+  			    closeTabsRightOfCurrent(getFocusedWindow().tabs);
+  			  };
+
+  			  setupConnection();
+
+  			  if (widget.preferences.getItem('option') == undefined)
+  			    widget.preferences.setItem('option', '2'); //default: right
+
+  			  if (widget.preferences.getItem('includeSelf') == undefined)
+  			    widget.preferences.setItem('includeSelf', 0); //default: no
+
+  			  resetButtons();
+
+  			}, false);
 
 });
