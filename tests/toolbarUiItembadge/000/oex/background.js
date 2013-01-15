33680bae2c1860956d7ba7604f134e9f3aaae3fd
Michalina Ziolkowska <mziolkowska@opera.com>
2013-01-15 09:20:36 +0100

    tc fixed

--- tests/toolbarUiItembadge/000/oex/background.js
+++ tests/toolbarUiItembadge/000/oex/background.js
@@ -1,30 +1,34 @@
-var timer = null, counter = 0;
-var theButton;
-window.addEventListener("load", function(){
-    var UIItemProperties = {
-      disabled: false,
-      title: "000 - exists",
-      icon: "./oex/icon.png",
-      onclick: function(){
-        if( timer ){
-          window.clearTimeout( timer );
-          timer = null;
-        } else {
-          timer = window.setInterval( function(){
-              MANUAL( "Changing theButton.badge.textContent to Counter = " + counter );
-              theButton.badge.textContent = "No.:" + counter;
-              counter++;
-          }, 500);
+opera.isReady(function(){
+    var timer = null, counter = 0;
+    var theButton;
+
+    window.addEventListener("load", function(){
+        var UIItemProperties = {
+          disabled: false,
+          title: "000 - exists",
+          icon: "./oex/icon.png",
+          onclick: function(){
+            if( timer ){
+              window.clearTimeout( timer );
+              timer = null;
+            } else {
+              timer = window.setInterval( function(){
+                  MANUAL( "Changing theButton.badge.textContent to Counter = " + counter );
+                  theButton.badge.textContent = "" + counter;
+                  counter++;
+              }, 500);
+            }
+          },
+          badge: {
+            textContent: 'Description',
+            backgroundColor: '#ffeedd',
+            color: '#404040',
+            display: 'hidden'
+          }
         }
-      },
-      badge: {
-        textContent: 'Description',
-        backgroundColor: '#ffeedd',
-        color: '#404040',
-        display: 'hidden'
-      }
-    }
-    theButton = opera.contexts.toolbar.createItem( UIItemProperties );
-    opera.contexts.toolbar.addItem( theButton );
-    MANUAL( "If there is an enabled button with a title and favicon, click the button to change badge.textContent" );
-}, false);
+        theButton = opera.contexts.toolbar.createItem( UIItemProperties );
+        opera.contexts.toolbar.addItem( theButton );
+        MANUAL( "If there is an enabled button with a title and favicon, click the button to change badge.textContent" );
+    }, false);
+
+});
