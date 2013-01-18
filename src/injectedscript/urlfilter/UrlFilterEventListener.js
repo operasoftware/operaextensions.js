2fd6fc0bf6b7f6c637a1c78f10bff9e7c377f9c2
richtr <rich.tibbett@gmail.com>
2013-01-18 15:42:22 +0100

    Refactor of URLFilterEventListener

--- src/injectedscript/urlfilter/UrlFilterEventListener.js
+++ src/injectedscript/urlfilter/UrlFilterEventListener.js
@@ -7,7 +7,7 @@ var UrlFilterEventListener = function() {
   this.pageSrcElements = {};
   
   // Catch resource load failures and reconcile with incoming event messages from background
-  global.addEventListener('load', function(evt) {
+  this.grabPageElements = function() {
 
     // Catch static HTML elements that load external content via 'src' tag: 
     // SCRIPT (script), IMAGE (img, image), STYLESHEET (link rel='stylesheet'), 
@@ -29,146 +29,74 @@ var UrlFilterEventListener = function() {
       this.pageSrcElements[ key ].push( els[i] );
     }
     
-  }.bind(this), false);
+  };
   
-  /*Object.defineProperty(this, 'matchUrlToInPageElement', {
-    enumerable: false,  
-    configurable: false, 
-    writable: false, 
-    value: function( url ) {
-      // Strip # seperator from URL (since it is not provided as part of any blocked path URL)
-      url = url.split("#")[0];
-      
-      var key = global.encodeURIComponent( url );
-      var pos = this.pageSrcElementsPointers[key];
-      
-      if( pos === undefined ) {
-        pos = this.pageSrcElementsPointers[key] = 0;
-      }
+  this.dispatchURLFilterEvent = function( type, data ) {
     
-      if( this.pageSrcElements[key] !== undefined && this.pageSrcElements[key].length > 0 ) {
+    // get a set of the latest page elements
+    this.pageSrcElements = {};
+    this.grabPageElements.call(this);
+    
+    type = type + ""; // force 'type' to string
+    data = data || { 'url': 'null' };
+    
+    var key = global.encodeURIComponent( (data.url).split('#')[0] );
+    
+    if( this.pageSrcElements[ key ] == undefined || this.pageSrcElements[ key ] == null ) {
       
-        var el = this.pageSrcElements[key][pos];
+      // Fire 1 basic contentblocked event on this object
+      this.dispatchEvent( new OEvent(type, data) );
+      
+    } else {
+  
+      for(var i = 0, l = this.pageSrcElements[ key ].length; i < l; i++) {
+    
+        var evtData = data;
+
+        // Reconcile element from blocked url
+        evtData.element = this.pageSrcElements[ key ][ i ];
         
-        if(this.pageSrcElements[key].length > pos) {
-          pos = this.pageSrcElementsPointers[key] += 1;
-        }
+        // Re-write correct URL for contentblocked event
+        evtData.url = evtData.element ? evtData.element.origUrl : evtData.url;
+
+        // Fire contentblocked event on this object
+        this.dispatchEvent( new OEvent(type, evtData) );
       
-        return el;
+      }
       
-      } 
+      this.pageSrcElements[ key ] = [];
     
-      return undefined; // default, not found
     }
-  });*/
+    
+  }
 
   // listen for block events sent from the background process
   // and fire in this content script
 
   OEX.addEventListener('controlmessage', function( msg ) {
 
-    if( !msg.data || !msg.data.action ) {
+    if( !msg.data || !msg.data.action || !msg.data.data ) {
       return;
     }
-    
-    msg.data.data = msg.data.data || {};
     
     switch( msg.data.action ) {
 
       // Set up all storage properties
       case '___O_urlfilter_contentblocked':
       
-        var key = global.encodeURIComponent( (msg.data.data.url).split('#')[0] );
-        
-        if( this.pageSrcElements[ key ] == undefined || this.pageSrcElements[ key ] == null ) {
-          
-          // Fire 1 basic contentblocked event on this object
-          this.dispatchEvent( new OEvent('contentblocked', msg.data.data) );
-          
-        } else {
-      
-          for(var i = 0, l = this.pageSrcElements[ key ].length; i < l; i++) {
-        
-            var evtData = msg.data.data;
-
-            // Reconcile element from blocked url
-            evtData.element = this.pageSrcElements[ key ][ i ];
-            
-            // Re-write correct URL for contentblocked event
-            evtData.url = evtData.element ? evtData.element.origUrl : evtData.url;
-
-            // Fire contentblocked event on this object
-            this.dispatchEvent( new OEvent('contentblocked', evtData) );
-          
-          }
-          
-          this.pageSrcElements[ key ] = [];
-        
-        }
+        this.dispatchURLFilterEvent.call(this, 'contentblocked', msg.data.data)
 
         break;
 
       case '___O_urlfilter_contentunblocked':
       
-        var key = global.encodeURIComponent( (msg.data.data.url).split('#')[0] );
-      
-        if( this.pageSrcElements[ key ] == undefined || this.pageSrcElements[ key ] == null ) {
-        
-          // Fire 1 basic contentblocked event on this object
-          this.dispatchEvent( new OEvent('contentunblocked', msg.data.data) );
-        
-        } else {
-    
-          for(var i = 0, l = this.pageSrcElements[ key ].length; i < l; i++) {
-      
-            var evtData = msg.data.data;
-
-            // Reconcile element from blocked url
-            evtData.element = this.pageSrcElements[ key ][ i ];
-          
-            // Re-write correct URL for contentblocked event
-            evtData.url = evtData.element ? evtData.element.origUrl : evtData.url;
-
-            // Fire contentblocked event on this object
-            this.dispatchEvent( new OEvent('contentunblocked', evtData) );
-        
-          }
-          
-          this.pageSrcElements[ key ] = [];
-      
-        }
+        this.dispatchURLFilterEvent.call(this, 'contentunblocked', msg.data.data)
 
         break;
         
       case '___O_urlfilter_contentallowed':
       
-        var key = global.encodeURIComponent( (msg.data.data.url).split('#')[0] );
-      
-        if( this.pageSrcElements[ key ] == undefined || this.pageSrcElements[ key ] == null ) {
-        
-          // Fire 1 basic contentblocked event on this object
-          this.dispatchEvent( new OEvent('contentallowed', msg.data.data) );
-        
-        } else {
-    
-          for(var i = 0, l = this.pageSrcElements[ key ].length; i < l; i++) {
-      
-            var evtData = msg.data.data;
-
-            // Reconcile element from blocked url
-            evtData.element = this.pageSrcElements[ key ][ i ];
-          
-            // Re-write correct URL for contentblocked event
-            evtData.url = evtData.element ? evtData.element.origUrl : evtData.url;
-
-            // Fire contentblocked event on this object
-            this.dispatchEvent( new OEvent('contentallowed', evtData) );
-        
-          }
-          
-          this.pageSrcElements[ key ] = [];
-      
-        }
+        this.dispatchURLFilterEvent.call(this, 'contentallowed', msg.data.data)
 
         break;
     }
@@ -181,6 +109,7 @@ UrlFilterEventListener.prototype = Object.create( OEventTarget.prototype );
 
 // Override
 UrlFilterEventListener.prototype.addEventListener = function(eventName, callback, useCapture) {
+  
   eventName = (eventName + "").toLowerCase(); // force to lower case string
   
   this.on(eventName, callback); // no useCapture
