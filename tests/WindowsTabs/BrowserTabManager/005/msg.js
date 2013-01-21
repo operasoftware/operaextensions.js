f951ebfadbd1f19e388aed38071ed3dc406d61c7
Michalina Ziolkowska <mziolkowska@opera.com>
2013-01-21 09:46:29 +0100

    _manager ext updated

--- tests/WindowsTabs/BrowserTabManager/005/msg.js
+++ tests/WindowsTabs/BrowserTabManager/005/msg.js
@@ -1,8 +1,10 @@
 var senderID = "nandnlijbdgklpggddgnojncccikgcfa";
-add_completion_callback(function(tests, status) {
-    var log = document.querySelector("#log #results");
-    var msg = encodeURIComponent(log.innerHTML);
-    if(senderID) {
-	chrome.extension.sendMessage(senderID, msg);
-    }
-});
+if(chrome != "undefined") {
+    add_completion_callback(function(tests, status) {
+        var log = document.querySelector("#log #results");
+        var msg = encodeURIComponent(log.innerHTML);
+        if (senderID) {
+            chrome.extension.sendMessage(senderID, msg);
+        }
+    });
+}
