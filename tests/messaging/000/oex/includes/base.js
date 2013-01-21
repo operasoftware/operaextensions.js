a55a6c477cf5109582f7bd1561afa593fb937858
Michalina Ziolkowska <mziolkowska@opera.com>
2013-01-21 13:17:09 +0100

    fixed messaging-000 tc, checking message instead of event.source object

--- tests/messaging/000/oex/includes/base.js
+++ tests/messaging/000/oex/includes/base.js
@@ -1,76 +1,75 @@
-//opera.isReady(function() {
-  
-  window.addEventListener( "load", function( event ){
-      if( opera.extension ){
+opera.isReady(function(){
+    window.addEventListener( "load", function( event ){
+        if( opera.extension ){
 
-  	opera.extension.onmessage = function( event ){
-  	    MANUAL( "Event received by the helper from " + event.origin );
-  	    if( event.ports ){
-  		MANUAL( "Responding to port. Ports are:\n" + getProperties( event.ports, 2 ) );
-  		event.ports[0].postMessage( "Hi" );
-  	    } else if(event.source){
-  		MANUAL( "Responding to source" );
-  		event.source.postMessage( "Hi" );
-  	    } else {
-  		MANUAL( "Unable to use event.source, trying opera.extension.postMessage." );
-  		opera.extension.postMessage("heyhey");
-  	    }
-  	}
-    } else {
-        FAIL( "Couldn't find an extension or opera.extension object." );
-    }
-  }, false);
-  // The base document. The userJS for the test is attached above this point.
-  function getProperties( object, depth, prefix )
-  {
-      prefix = prefix||"";
-      var t = "";
-      for( key in object )
-      {
-          try{
-            t += prefix + "" + "[" + key  + "] " + typeofobject[key] + " " + object[key] + "\n";
-            if( typeofobject[key] == "object" && depth>0 )
-            {
-                t += getProperties( object[key], depth-1, "[" + key  + "]" );
-            }
-            if( depth==0 ){
-                t += "------------ Depth limit reached. \n";
-            }
-          } catch (e){
-            t += getProperties( e, 5 ) + "\n";
-          }
+    	opera.extension.onmessage = function( event ){
+    	    MANUAL( "Event received by the helper from " + event.origin );
+    	    if( event.ports ){
+    		MANUAL( "Responding to port. Ports are:\n" + getProperties( event.ports, 2 ) );
+    		event.ports[0].postMessage( "Responding to port" );
+    	    } else if(event.source){
+    		MANUAL( "Responding to source" );
+    		event.source.postMessage( "Responding to source" );
+    	    } else {
+    		MANUAL( "Unable to use event.source, trying opera.extension.postMessage." );
+    		opera.extension.postMessage("Posting message");
+    	    }
+    	}
+      } else {
+          FAIL( "Couldn't find an extension or opera.extension object." );
       }
-      return t;
-  }
-  var postToLocalHost = false;
-  function POST( result, msg )
-  {
-    msg = msg||"";
-    var value = "Extensions: 000 - back broadCast userjs \t" + result + "\n" + msg;
-    var req = new window.XMLHttpRequest();
-    if( postToLocalHost )
-    {
-      params = "jstests[]=" + value;
-      req.open( "http://localhost:8840", "POST");
-      req.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-      req.setRequestHeader("Content-length", params.length);
-      req.setRequestHeader("Connection", "close");
-      req.send( params );
-    } else {
-      if( window.opera ){window.opera.postError( "==userJS==\n" + value );}
-    }
-  }
-  function PASS( msg )
-  {
-      POST( "PASSED", msg );
-  }
-  function FAIL( msg )
-  {
-      POST( "FAILED", msg );
-  }
-  function MANUAL( msg )
-  {
-      POST( "MANUAL", msg );
-  }
+    }, false);
+});
 
-//});
+// The base document. The userJS for the test is attached above this point.
+function getProperties( object, depth, prefix )
+{
+    prefix = prefix||"";
+    var t = "";
+    for( key in object )
+    {
+        try{
+          t += prefix + "" + "[" + key  + "] " + typeofobject[key] + " " + object[key] + "\n";
+          if( typeofobject[key] == "object" && depth>0 )
+          {
+              t += getProperties( object[key], depth-1, "[" + key  + "]" );
+          }
+          if( depth==0 ){
+              t += "------------ Depth limit reached. \n";
+          }
+        } catch (e){
+          t += getProperties( e, 5 ) + "\n";
+        }
+    }
+    return t;
+}
+var postToLocalHost = false;
+function POST( result, msg )
+{
+  msg = msg||"";
+  var value = "Extensions: 000 - back broadCast userjs \t" + result + "\n" + msg;
+  var req = new window.XMLHttpRequest();
+  if( postToLocalHost )
+  {
+    params = "jstests[]=" + value;
+    req.open( "http://localhost:8840", "POST");
+    req.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    req.setRequestHeader("Content-length", params.length);
+    req.setRequestHeader("Connection", "close");
+    req.send( params );
+  } else {
+    if( window.opera ){window.opera.postError( "==userJS==\n" + value );}
+  }
+}
+function PASS( msg )
+{
+    POST( "PASSED", msg );
+}
+function FAIL( msg )
+{
+    POST( "FAILED", msg );
+}
+function MANUAL( msg )
+{
+    POST( "MANUAL", msg );
+}
