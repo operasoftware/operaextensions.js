56741bdbdd9a878b0858ddad885790c9b56643e6
Rich Tibbett <richt@opera.com>
2013-01-21 16:16:37 +0100

    Fix /tests/toolbarUiItempopup/004/ so it executes

--- tests/toolbarUiItempopup/004/oex/background.js
+++ tests/toolbarUiItempopup/004/oex/background.js
@@ -9,7 +9,7 @@ opera
 				var UIItemProperties = {
 				    disabled : false,
 				    title : "004 - createItem datauri href",
-				    icon : "icon.png",
+				    icon : "/oex/icon.png",
 				    popup : {
 					href : "data:text/html;charset=utf-8,%3CDOCTYPE%20HTML%3E%3Chtml%3E%3Chead%3E%3Ctitle%3ETest%3C%2Ftitle%3E%3Cmeta%20http-equiv%3D%22refresh%22%20content%3D%222%3Burl%3Dhttp%3A%2F%2Fwww.opera.com%2F%22%3E%3C%2Fhead%3E%3Cbody%3E%3Cp%3ETest.%20Should%20reload%20to%20opera.com%3C%2Fp%3E%3C%2Fbody%3E%3C%2Fhtml%3E%0D%0A",
 					width : 100,
