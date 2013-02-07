629cf6ec5dd5308a1d24234950943e9a6ca676f2
Rich Tibbett <richt@opera.com>
2013-02-07 15:32:48 +0100

    Add a broader set of events for inline event handler rewrite function

--- src/common/InlineEventHandlerRewrite.js
+++ src/common/InlineEventHandlerRewrite.js
@@ -1,22 +1,27 @@
 
-// Rewrite in-line event handlers (eg. <input ... onclick=""> for a sub-set of common standard events)
+opera.isReady(function() {
 
-document.addEventListener('DOMContentLoaded', function(e) {
+  // Rewrite in-line event handlers (eg. <input ... onclick=""> for a sub-set of common standard events)
+
+  document.addEventListener('DOMContentLoaded', function(e) {
   
-  var selectors = ['load', 'click', 'mouseover', 'mouseout', 'keydown', 'keypress', 'keyup', 'blur', 'focus'];
+    var selectors = ['load', 'beforeunload', 'unload', 'click', 'dblclick', 'mouseover', 'mousemove', 
+                        'mousedown', 'mouseup', 'mouseout', 'keydown', 'keypress', 'keyup', 'blur', 'focus'];
 
-  for(var i = 0, l = selectors.length; i < l; i++) {
-    var els = document.querySelectorAll('[on' + selectors[i] + ']');
-    for(var j = 0, k = els.length; j < k; j++) {
-      var fn = new Function('e', els[j].getAttribute('on' + selectors[i]));
-      var target = els[j];
-      if(selectors[i] === 'load' && els[j] === document.body) {
-        target = window;
-      }
+    for(var i = 0, l = selectors.length; i < l; i++) {
+      var els = document.querySelectorAll('[on' + selectors[i] + ']');
+      for(var j = 0, k = els.length; j < k; j++) {
+        var fn = new Function('e', els[j].getAttribute('on' + selectors[i]));
+        var target = els[j];
+        if(selectors[i].indexOf('load') > -1 && els[j] === document.body) {
+          target = window;
+        }
       
-      els[j].removeAttribute('on' + selectors[i]);
-      target.addEventListener(selectors[i], fn, true);
+        els[j].removeAttribute('on' + selectors[i]);
+        target.addEventListener(selectors[i], fn, true);
+      }
     }
-  }
   
-}, false);
+  }, false);
+
+});
