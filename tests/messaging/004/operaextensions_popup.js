fb1f21040b9ce8e14ce7760b7a3334fb0bff0964
Rich Tibbett <richt@opera.com>
2013-02-11 13:24:58 +0100

    Update build scripts

--- tests/messaging/004/operaextensions_popup.js
+++ tests/messaging/004/operaextensions_popup.js
@@ -815,238 +815,234 @@ if(manifest && manifest.permissions && manifest.permissions.indexOf('contextMenu
 
 }
 
-  if (global.opera) {
-    isReady = true;
+if (global.opera) {
+  isReady = true;
 
-    // Make scripts also work in Opera <= version 12
-    opera.isReady = function(fn) {
-      fn.call(opera);
+  // Make scripts also work in Opera <= version 12
+  opera.isReady = function(fn) {
+    fn.call(opera);
 
-      // Run delayed events (if any)
-      for(var i = 0, l = _delayedExecuteEvents.length; i < l; i++) {
-        var o = _delayedExecuteEvents[i];
-        o.target[o.methodName].apply(o.target, o.args);
-      }
-      _delayedExecuteEvents = [];
-    };
+    // Run delayed events (if any)
+    for(var i = 0, l = _delayedExecuteEvents.length; i < l; i++) {
+      var o = _delayedExecuteEvents[i];
+      o.target[o.methodName].apply(o.target, o.args);
+    }
+    _delayedExecuteEvents = [];
+  };
 
-  } else {
+} else {
 
-    opera.isReady = (function() {
+  opera.isReady = (function() {
 
-      var fns = {
-            "isready": [],
-            "readystatechange": [],
-            "domcontentloaded": [],
-            "load": []
-          };
-
-      var hasFired_DOMContentLoaded = false,
-          hasFired_Load = false;
-
-      global.document.addEventListener("DOMContentLoaded", function handle_DomContentLoaded() {
-        hasFired_DOMContentLoaded = true;
-        global.document.removeEventListener("DOMContentLoaded", handle_DomContentLoaded, true);
-      }, true);
-
-      global.addEventListener("load", function handle_Load() {
-        hasFired_Load = true;
-        global.removeEventListener("load", handle_Load, true);
-      }, true);
-      
-      global.document.addEventListener("readystatechange", function(event) {
-        event.stopImmediatePropagation();
-        event.stopPropagation();
-        if( global.document.readyState !== 'interactive' && global.document.readyState !== 'complete' ) {
-          fireEvent('readystatechange', global.document);
-        } else {
-          global.document.readyState = 'loading';
-        }
-      }, true);
-      
-      var _readyState = "uninitialized";
-      global.document.__defineSetter__('readyState', function(val) { _readyState = val; });
-      global.document.__defineGetter__('readyState', function() { return _readyState; });
-
-      function interceptAddEventListener(target, _name) {
-
-        var _target = target.addEventListener;
-
-        // Replace addEventListener for given target
-        target.addEventListener = function(name, fn, usecapture) {
-          name = name + ""; // force event name to type string
-          
-          if (name.toLowerCase() === _name.toLowerCase()) {
-            if (fn === undefined || fn === null ||
-                  Object.prototype.toString.call(fn) !== "[object Function]") {
-              return;
-            }
-
-            if (isReady) {
-              fn.call(global);
-            } else {
-              fns[_name.toLowerCase()].push(fn);
-            }
-          } else {
-            // call standard addEventListener method on target
-            _target.call(target, name, fn, usecapture);
-          }
+    var fns = {
+          "isready": [],
+          "readystatechange": [],
+          "domcontentloaded": [],
+          "load": []
         };
 
-        // Replace target.on[_name] with custom setter function
-        target.__defineSetter__("on" + _name.toLowerCase(), function( fn ) {
-          // call code block just created above...
-          target.addEventListener(_name.toLowerCase(), fn, false);
-        });
+    var hasFired_DOMContentLoaded = false,
+        hasFired_Load = false;
 
+    global.document.addEventListener("DOMContentLoaded", function handle_DomContentLoaded() {
+      hasFired_DOMContentLoaded = true;
+      global.document.removeEventListener("DOMContentLoaded", handle_DomContentLoaded, true);
+    }, true);
+
+    global.addEventListener("load", function handle_Load() {
+      hasFired_Load = true;
+      global.removeEventListener("load", handle_Load, true);
+    }, true);
+    
+    global.document.addEventListener("readystatechange", function(event) {
+      event.stopImmediatePropagation();
+      event.stopPropagation();
+      if( global.document.readyState !== 'interactive' && global.document.readyState !== 'complete' ) {
+        fireEvent('readystatechange', global.document);
+      } else {
+        global.document.readyState = 'loading';
       }
+    }, true);
+    
+    var _readyState = "uninitialized";
+    global.document.__defineSetter__('readyState', function(val) { _readyState = val; });
+    global.document.__defineGetter__('readyState', function() { return _readyState; });
 
-      interceptAddEventListener(global, 'load');
-      interceptAddEventListener(global.document, 'domcontentloaded');
-      interceptAddEventListener(global, 'domcontentloaded'); // handled bubbled DOMContentLoaded
-      interceptAddEventListener(global.document, 'readystatechange');
+    function interceptAddEventListener(target, _name) {
 
-      function fireEvent(name, target, props) {
-        var evtName = name.toLowerCase();
+      var _target = target.addEventListener;
 
-        var evt = new OEvent(evtName, props || {});
-
-        for (var i = 0, len = fns[evtName].length; i < len; i++) {
-          fns[evtName][i].call(target, evt);
-        }
-      }
-
-      function ready() {
-        global.setTimeout(function() {
-
-          if (isReady) {
+      // Replace addEventListener for given target
+      target.addEventListener = function(name, fn, usecapture) {
+        name = name + ""; // force event name to type string
+        
+        if (name.toLowerCase() === _name.toLowerCase()) {
+          if (fn === undefined || fn === null ||
+                Object.prototype.toString.call(fn) !== "[object Function]") {
             return;
           }
 
-          // Handle queued opera 'isReady' event functions
-          for (var i = 0, len = fns['isready'].length; i < len; i++) {
-            fns['isready'][i].call(global);
+          if (isReady) {
+            fn.call(global);
+          } else {
+            fns[_name.toLowerCase()].push(fn);
           }
-          fns['isready'] = []; // clear
+        } else {
+          // call standard addEventListener method on target
+          _target.call(target, name, fn, usecapture);
+        }
+      };
 
-          var domContentLoadedTimeoutOverride = new Date().getTime() + 3000;
+      // Replace target.on[_name] with custom setter function
+      target.__defineSetter__("on" + _name.toLowerCase(), function( fn ) {
+        // call code block just created above...
+        target.addEventListener(_name.toLowerCase(), fn, false);
+      });
 
-          // Synthesize and fire the document domcontentloaded event
-          (function fireDOMContentLoaded() {
+    }
 
-            var currentTime = new Date().getTime();
+    interceptAddEventListener(global, 'load');
+    interceptAddEventListener(global.document, 'domcontentloaded');
+    interceptAddEventListener(global, 'domcontentloaded'); // handled bubbled DOMContentLoaded
+    interceptAddEventListener(global.document, 'readystatechange');
 
-            // Check for hadFired_Load in case we missed DOMContentLoaded
-            // event, in which case, we syntesize DOMContentLoaded here
-            // (always synthesized in Chromium Content Scripts)
-            if (hasFired_DOMContentLoaded || hasFired_Load || currentTime >= domContentLoadedTimeoutOverride) {
+    function fireEvent(name, target, props) {
+      var evtName = name.toLowerCase();
 
-              global.document.readyState = 'interactive';
-              fireEvent('readystatechange', global.document);
+      var evt = new OEvent(evtName, props || {});
 
-              fireEvent('domcontentloaded', global.document, { bubbles: true }); // indicate that event bubbles
-
-              if(currentTime >= domContentLoadedTimeoutOverride) {
-                console.warn('document.domcontentloaded event fired on check timeout');
-              }
-
-              var loadTimeoutOverride = new Date().getTime() + 3000;
-
-              // Synthesize and fire the window load event
-              // after the domcontentloaded event has been
-              // fired
-              (function fireLoad() {
-
-                var currentTime = new Date().getTime();
-
-                if (hasFired_Load || currentTime >= loadTimeoutOverride) {
-                  
-                  global.document.readyState = 'complete';
-                  fireEvent('readystatechange', global.document);
-
-                  fireEvent('load', window);
-
-                  if(currentTime >= loadTimeoutOverride) {
-                    console.warn('window.load event fired on check timeout');
-                  }
-
-                  // Run delayed events (if any)
-                  for(var i = 0, l = _delayedExecuteEvents.length; i < l; i++) {
-                    var o = _delayedExecuteEvents[i];
-                    o.target[o.methodName].apply(o.target, o.args);
-                  }
-                  _delayedExecuteEvents = [];
-
-                } else {
-                  global.setTimeout(function() {
-                    fireLoad();
-                  }, 50);
-                }
-
-              })();
-
-            } else {
-              global.setTimeout(function() {
-                fireDOMContentLoaded();
-              }, 50);
-            }
-
-          })();
-
-          isReady = true;
-
-        }, 0);
+      for (var i = 0, len = fns[evtName].length; i < len; i++) {
+        fns[evtName][i].call(target, evt);
       }
+    }
 
-      var holdTimeoutOverride = new Date().getTime() + 3000;
+    function ready() {
+      global.setTimeout(function() {
 
-      (function holdReady() {
-
-        var currentTime = new Date().getTime();
-
-        if (currentTime >= holdTimeoutOverride) {
-          // All scripts now ready to be executed: TIMEOUT override
-          console.warn('opera.isReady check timed out');
-          hasFired_Load = true; // override
-          ready();
+        if (isReady) {
           return;
         }
 
-        for (var i in deferredComponentsLoadStatus) {
-          if (deferredComponentsLoadStatus[i] !== true) {
-            // spin the loop until everything is working
-            // or we receive a timeout override (handled
-            // in next loop, above)
-            global.setTimeout(function() {
-              holdReady();
-            }, 20);
-            return;
-          }
+        // Handle queued opera 'isReady' event functions
+        for (var i = 0, len = fns['isready'].length; i < len; i++) {
+          fns['isready'][i].call(global);
         }
+        fns['isready'] = []; // clear
 
-        // All scripts now ready to be executed
+        var domContentLoadedTimeoutOverride = new Date().getTime() + 3000;
+
+        // Synthesize and fire the document domcontentloaded event
+        (function fireDOMContentLoaded() {
+
+          var currentTime = new Date().getTime();
+
+          // Check for hadFired_Load in case we missed DOMContentLoaded
+          // event, in which case, we syntesize DOMContentLoaded here
+          // (always synthesized in Chromium Content Scripts)
+          if (hasFired_DOMContentLoaded || hasFired_Load || currentTime >= domContentLoadedTimeoutOverride) {
+
+            global.document.readyState = 'interactive';
+            fireEvent('readystatechange', global.document);
+
+            fireEvent('domcontentloaded', global.document, { bubbles: true }); // indicate that event bubbles
+
+            if(currentTime >= domContentLoadedTimeoutOverride) {
+              console.warn('document.domcontentloaded event fired on check timeout');
+            }
+
+            var loadTimeoutOverride = new Date().getTime() + 3000;
+
+            // Synthesize and fire the window load event
+            // after the domcontentloaded event has been
+            // fired
+            (function fireLoad() {
+
+              var currentTime = new Date().getTime();
+
+              if (hasFired_Load || currentTime >= loadTimeoutOverride) {
+                
+                global.document.readyState = 'complete';
+                fireEvent('readystatechange', global.document);
+
+                fireEvent('load', window);
+
+                if(currentTime >= loadTimeoutOverride) {
+                  console.warn('window.load event fired on check timeout');
+                }
+
+                // Run delayed events (if any)
+                for(var i = 0, l = _delayedExecuteEvents.length; i < l; i++) {
+                  var o = _delayedExecuteEvents[i];
+                  o.target[o.methodName].apply(o.target, o.args);
+                }
+                _delayedExecuteEvents = [];
+
+              } else {
+                global.setTimeout(function() {
+                  fireLoad();
+                }, 50);
+              }
+
+            })();
+
+          } else {
+            global.setTimeout(function() {
+              fireDOMContentLoaded();
+            }, 50);
+          }
+
+        })();
+
+        isReady = true;
+
+      }, 0);
+    }
+
+    var holdTimeoutOverride = new Date().getTime() + 3000;
+
+    (function holdReady() {
+
+      var currentTime = new Date().getTime();
+
+      if (currentTime >= holdTimeoutOverride) {
+        // All scripts now ready to be executed: TIMEOUT override
+        console.warn('opera.isReady check timed out');
+        hasFired_Load = true; // override
         ready();
+        return;
+      }
 
-      })();
-
-      return function(fn) {
-        // if the Library is already ready,
-        // execute the function immediately.
-        // otherwise, queue it up until isReady
-        if (isReady) {
-          fn.call(global);
-        } else {
-          fns['isready'].push(fn);
+      for (var i in deferredComponentsLoadStatus) {
+        if (deferredComponentsLoadStatus[i] !== true) {
+          // spin the loop until everything is working
+          // or we receive a timeout override (handled
+          // in next loop, above)
+          global.setTimeout(function() {
+            holdReady();
+          }, 20);
+          return;
         }
       }
+
+      // All scripts now ready to be executed
+      ready();
+
     })();
 
-  }
+    return function(fn) {
+      // if the Library is already ready,
+      // execute the function immediately.
+      // otherwise, queue it up until isReady
+      if (isReady) {
+        fn.call(global);
+      } else {
+        fns['isready'].push(fn);
+      }
+    }
+  })();
 
-  // Make API available on the window DOM object
-  global.opera = opera;
+}
 
-})( window );
 // Set the width and height of the popup window to values provided in the URL query string
 opera.isReady(function() {
 
@@ -1100,3 +1096,8 @@ opera.isReady(function() {
   }, false);
 
 });
+
+  // Make API available on the window DOM object
+  global.opera = opera;
+
+})( window );
