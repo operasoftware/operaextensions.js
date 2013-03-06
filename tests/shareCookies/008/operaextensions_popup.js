1e87b3dbd4dd1efe38b1db28a04bc9f3bc05fc05
Rich Tibbett <richt@opera.com>
2013-03-06 15:53:01 +0100

    Update build scripts

--- tests/shareCookies/008/operaextensions_popup.js
+++ tests/shareCookies/008/operaextensions_popup.js
@@ -1,4 +1,13 @@
 !(function( global ) {
+  
+  // NEX<->CRX support set up
+  var nexAPIStubs = ['app', 'extension', 'windows', 'tabs', 'browserAction', 'contextMenus', 'i18n', 'webRequest', ''];
+  if(!global.chrome) {
+    global.chrome = {};
+  }
+  for(var i = 0, l = nexAPIStubs.length; i < l; i++) {
+    global.chrome[ nexAPIStubs[ i ] ] = global.navigator[ nexAPIStubs[ i ] ] || {};
+  }
 
   var Opera = function() {};
 
