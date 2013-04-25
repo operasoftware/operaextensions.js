b10e237e5c1e75301700d00405bc707fbfb04a60
Rich Tibbett <richt@opera.com>
2013-04-25 08:28:25 +0200

    Update build scripts

--- tests/toolbarUiItempopup/005/operaextensions_popup.js
+++ tests/toolbarUiItempopup/005/operaextensions_popup.js
@@ -515,15 +515,28 @@ function OError(name, msg, code) {
 OError.prototype.__proto__ = Error.prototype;
 
 var OEvent = function(eventType, eventProperties) {
-
-  var evt = document.createEvent("Event");
+  
+  var evt = eventProperties || {};
+  
+  evt.type = eventType;
+        
+  if(!evt.target) evt.target = global;
+  if(!evt.currentTarget) evt.currentTarget = evt.target;
+  if(!evt.srcElement) evt.srcElement = evt.target;
+  
+  if(evt.bubbles !== true) evt.bubbles = false;
+  if(evt.cancelable !== true) evt.cancelable = false;
+  
+  if(!evt.timeStamp) evt.timeStamp = 0;
+  
+  /*var evt = document.createEvent("Event");
 
   evt.initEvent(eventType, true, true);
 
   // Add custom properties or override standard event properties
   for (var i in eventProperties) {
     evt[i] = eventProperties[i];
-  }
+  }*/
 
   return evt;
 
