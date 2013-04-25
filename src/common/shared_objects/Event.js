7e3aff02939ab500fe85d56e96f7e89cfa216f0d
Rich Tibbett <richt@opera.com>
2013-04-25 11:27:47 +0200

    Reinstate Event handling but make an exception for isReady generated events (so we can specifically set evt.target, etc)

--- src/common/shared_objects/Event.js
+++ src/common/shared_objects/Event.js
@@ -1,28 +1,14 @@
 
 var OEvent = function(eventType, eventProperties) {
   
-  var evt = eventProperties || {};
+  var props = eventProperties || {};
   
-  evt.type = eventType;
-        
-  if(!evt.target) evt.target = global;
-  if(!evt.currentTarget) evt.currentTarget = evt.target;
-  if(!evt.srcElement) evt.srcElement = evt.target;
-  
-  if(evt.bubbles !== true) evt.bubbles = false;
-  if(evt.cancelable !== true) evt.cancelable = false;
-  
-  if(!evt.timeStamp) evt.timeStamp = 0;
-  
-  /*var evt = document.createEvent("Event");
+  var newEvt = new CustomEvent(eventType, true, true);
 
-  evt.initEvent(eventType, true, true);
+  for(var i in props) {
+    newEvt[i] = props[i];
+  }
 
-  // Add custom properties or override standard event properties
-  for (var i in eventProperties) {
-    evt[i] = eventProperties[i];
-  }*/
-
-  return evt;
+  return newEvt;
 
 };
