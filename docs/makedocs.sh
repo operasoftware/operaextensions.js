58404701e4967edad2b5d5bd48db38add505d8c5
Rich Tibbett <richt@opera.com>
2013-05-24 10:14:52 +0200

    Remove incomplete docs folder

--- docs/makedocs.sh
+++
@@ -1,9 +0,0 @@
-# Consult the README file @ ./resources/compiler/jsdocsgen/README
-#
-# You need to execute 'sudo cpan HTML::Template' before running this script!
-#
-perl ./resources/compiler/jsdocsgen/jsdoc.pl ./resources/refSrc/backgroundProcess/ --project-name "Opera Extensions - Background Process" --logo ./resources/images/opera.png --no-source -directory ./background/
-
-perl ./resources/compiler/jsdocsgen/jsdoc.pl ./resources/refSrc/userJS/ --project-name "Opera Extensions - Injected Scripts" --logo ./resources/images/opera.png --no-source -directory ./injectedscripts/
-
-perl ./resources/compiler/jsdocsgen/jsdoc.pl ./resources/refSrc/popup/ --project-name "Opera Extensions - Popup" --logo ./resources/images/opera.png --no-source -directory ./popup/
